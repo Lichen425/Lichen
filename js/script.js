330bd55d38785145f308740998c291750c23fe93
Lichen <lichen@192.168.5.106>
2020-01-09 20:16:55 +0800

    init

---
+++ js/script.js
@@ -0,0 +1,48 @@
+$(document).ready (function() {
+
+
+
+
+/* Scroll on buttons */
+
+$('.js--scroll-to-words').click(function () {
+    $('html, body').animate({scrollTop: $('.js--section-words').offset().top}, 1000);
+});
+
+
+});
+
+$('.js--scroll-to-words').click(function () {
+    $('h2').fadeIn (3000)
+
+});
+
+
+
+
+
+
+$('.js--scroll-to-idol').click(function () {
+    $('html, body').animate({scrollTop: $('.js--section-idol').offset().top}, 1000);
+});
+
+    
+
+
+
+$('.js--scroll-to-idol').click(function () {
+    $('img').fadeIn (3000)
+
+});
+
+
+
+
+
+
+
+
+   
+
+  
+
